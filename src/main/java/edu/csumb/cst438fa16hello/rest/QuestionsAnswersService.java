1b87b42a1ec52ae8f8e53652984f36b513adacf9
JoseCortez <jocortez@csumb.edu>
2016-09-12 17:25:52 -0700

    Update and rename HelloService.java to QuestionsAnswersService.java

--- src/main/java/edu/csumb/cst438fa16hello/rest/QuestionsAnswersService.java
+++ src/main/java/edu/csumb/cst438fa16hello/rest/QuestionsAnswersService.java
@@ -1,6 +1,6 @@
-package edu.csumb.cst438fa16hello.rest;
+package edu.csumb.cst438fa16QuestionsAnswers.rest;
 
-import edu.csumb.cst438fa16hello.DayOfWeek;
+import edu.csumb.cst438fa16QuestionsAnswers.QuestionsAnswers;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
@@ -13,16 +13,18 @@ import javax.ws.rs.core.Response;
  * https://jersey.java.net/documentation/latest/jaxrs-resources.html
  */
 @Path("/")
-public class HelloService {
+public class QuestionsAnswersService {
     @GET
     @Path("/today")
     public String today() {
-	return DayOfWeek.today();
+	return QuestionsAnswers.today();
     }
 
     @GET
     @Path("/hello")
     public Response hello(@QueryParam("name") String name) {
+    	//QuestionsAnswers.put()
+    	
         if (name == null || name.isEmpty()) {
             return Response.status(Response.Status.BAD_REQUEST).build();
         } else {
